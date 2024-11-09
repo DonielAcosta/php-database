3dc7da9f52028c21c533ad088ebcbd715f473c3d
Doniel Acosta <donielacosta1995@gmail.com>
2024-11-08 21:55:25 -0400

    struct enums and controllers

---
+++ app/Enums/PaymentTypeEnum.php
@@ -0,0 +1,8 @@
+<php
+namespace App\Enums;
+ enum PaymentTypeEnum: int{
+    case CreditCard =1;
+    case BankAccount =2;
+ }
+
+?>
--- database/PDO/Connection.php
+++ database/PDO/Connection.php
@@ -1,13 +1,36 @@
 <?php
-$server ="localhost";
-$database="finanzas_personales";
-$username="root";
-$password ="Donielacosta1995@";
+namespace Database\PDO;
+class Connection{
+    private static $instance;
+    private $conn;
 
+    private function __construct(){
+        $this->make_connection();
+    }
+    public static function getInstance(){
+        if(!self::$instance instanceof self){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    public function getDatabaseinstance(){
+        return $this->conn;
+    }
+    private function make_connection(){
+        $server ="localhost";
+        $database="finanzas_personales";
+        $username="root";
+        $password ="Donielacosta1995@";
+        
+        
+        $conecction = new \PDO("mysql:host=$server;dbname=$database",$username,$password);
+        //uso de caracteres
+        $setnames= $conecction->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
 
-$conecction = new PDO("mysql:host=$server;dbname=$database",$username,$password);
-//uso de caracteres
-$setnames= $conecction->prepare("SET NAMES 'utf8'");
-$setnames->execute();
-var_dump($setnames);
+        // var_dump($setnames);
+        $this->conn = $conecction;
+    }
+}
+
 ?>
